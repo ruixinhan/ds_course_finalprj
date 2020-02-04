a84bf3534f24d500c68ac6b603f225bc273e9206
Ruixin Han <rachelhrx@Ruixins-MacBook-Pro.local>
2020-02-04 14:30:36 -0600

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Create a directory for my data science course project with a standard project template using cookiecutter package',
+    author='ruixinhan',
+    license='MIT',
+)
